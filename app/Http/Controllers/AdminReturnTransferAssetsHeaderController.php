823eecfc472136e61720da208e5792f9f44ba9e9
DTCRepo <111104026+DTCRepo@users.noreply.github.com>
2023-01-30 12:52:22 +0800

    update print trf

--- app/Http/Controllers/AdminReturnTransferAssetsHeaderController.php
+++ app/Http/Controllers/AdminReturnTransferAssetsHeaderController.php
@@ -877,7 +877,6 @@
 					->leftjoin('mo_body_request', 'return_transfer_assets.mo_id', '=', 'mo_body_request.id')
 				->select(
 					'return_transfer_assets.*',
-					DB::raw('SUM(mo_body_request.unit_cost) as total_cost'),
 					'mo_body_request.*',
 					'statuses.*',
 					)
